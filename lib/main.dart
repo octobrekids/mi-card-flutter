8a234c2fa1658cb60835cd170c67947dfe5f7c34
octobrekids <chnrd.k@gmail.com>
2021-06-01 23:36:55 +0700

    flutter layout challenges

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,10 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
             child: Row(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
             Container(
-                height: 100.0,
+                height: double.infinity,
                 width: 100.0,
                 color: Colors.red,
                 child: Text('Container 1')),
@@ -23,12 +23,13 @@ class MyApp extends StatelessWidget {
             Container(
                 height: 100.0,
                 width: 100.0,
-                color: Colors.blue,
+                color: Colors.yellowAccent,
                 child: Text('Container 2')),
+            SizedBox(width: 20.0),
             Container(
-                height: 100.0,
+                height: double.infinity,
                 width: 100.0,
-                color: Colors.white,
+                color: Colors.blue,
                 child: Text('Container 3'))
           ],
         )),
