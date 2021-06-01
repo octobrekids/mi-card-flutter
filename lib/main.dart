9ed16dcd993cfb0b3c1164751ee4fa1a81ffb376
octobrekids <chnrd.k@gmail.com>
2021-06-02 00:08:17 +0700

    add text and circle avatar

--- lib/main.dart
+++ lib/main.dart
@@ -8,32 +8,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-            child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Container(
-                height: double.infinity,
-                width: 100.0,
-                color: Colors.red,
-                child: Text('Container 1')),
-            SizedBox(width: 20.0),
-            Container(
-                height: 100.0,
-                width: 100.0,
-                color: Colors.yellowAccent,
-                child: Text('Container 2')),
-            SizedBox(width: 20.0),
-            Container(
-                height: double.infinity,
-                width: 100.0,
-                color: Colors.blue,
-                child: Text('Container 3'))
-          ],
-        )),
+        home: Scaffold(
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+        child: Column(children: <Widget>[
+          CircleAvatar(
+            radius: 50.0,
+            backgroundImage: AssetImage('images/cat.jpg'),
+          ),
+          Text('Cat Meow',
+              style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold)),
+          Text('FLUTTER DEVELOPER',
+              style: TextStyle(
+                fontFamily: '',
+                fontSize: 20.0,
+                color: Colors.white,
+              ))
+        ]),
       ),
-    );
+    ));
   }
 }
