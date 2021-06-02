3ec220db33a2b03ada15e551fb6acaa4a67cbfe2
octobrekids <chnrd.k@gmail.com>
2021-06-02 14:01:29 +0700

    add divider line

--- lib/main.dart
+++ lib/main.dart
@@ -11,70 +11,74 @@ class MyApp extends StatelessWidget {
         home: Scaffold(
       backgroundColor: Colors.teal,
       body: SafeArea(
-        child: Column(children: <Widget>[
-          CircleAvatar(
-            radius: 50.0,
-            backgroundImage: AssetImage('images/cat.jpg'),
-          ),
-          Text('Cat Meow',
-              style: TextStyle(
-                  fontFamily: 'Pacifico',
-                  fontSize: 40.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold)),
-          Text(
-            'FLUTTER DEVELOPER',
-            style: TextStyle(
-                fontFamily: 'Source Sans Pro',
-                fontSize: 20.0,
-                color: Colors.white,
-                letterSpacing: 2.5,
-                fontWeight: FontWeight.bold),
-          ),
-          Card(
-            color: Colors.white,
-            margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.add_shopping_cart,
-                    color: Colors.teal.shade900,
-                  ),
-                  SizedBox(width: 10.0),
-                  Text(
-                    '+44 123 456 789',
-                    style: TextStyle(
-                        color: Colors.teal.shade900,
-                        fontFamily: 'Source Sans Pro',
-                        fontSize: 20.0),
-                  )
-                ],
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/cat.jpg'),
               ),
-            ),
-          ),
-          Card(
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              color: Colors.white,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(Icons.email, color: Colors.teal.shade900),
-                    SizedBox(
-                      width: 10.0,
+              Text('Cat Meow',
+                  style: TextStyle(
+                      fontFamily: 'Pacifico',
+                      fontSize: 40.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold)),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20.0,
+                    color: Colors.white,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(color: Colors.teal.shade100),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.add_shopping_cart,
+                      color: Colors.teal.shade900,
                     ),
-                    Text('chnrd.k@gmail.com',
-                        style: TextStyle(
+                    title: Text(
+                      '+44 123 456 789',
+                      style: TextStyle(
                           color: Colors.teal.shade900,
                           fontFamily: 'Source Sans Pro',
-                          fontSize: 20.0,
-                        ))
-                  ],
+                          fontSize: 20.0),
+                    ),
+                  ),
                 ),
-              ))
-        ]),
+              ),
+              Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  color: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        color: Colors.teal.shade900,
+                      ),
+                      title: Text(
+                        'chnrd.k@gmail.com',
+                        style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontFamily: 'Source Sans Pro',
+                            fontSize: 20.0),
+                      ),
+                    ),
+                  ))
+            ]),
       ),
     ));
   }
