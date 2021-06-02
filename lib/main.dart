25e553da25cdc93192ac2cb04a6a0a37ec69d4df
octobrekids <chnrd.k@gmail.com>
2021-06-02 12:39:43 +0700

    add email card

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,46 @@ class MyApp extends StatelessWidget {
                 color: Colors.white,
                 letterSpacing: 2.5,
                 fontWeight: FontWeight.bold),
-          )
+          ),
+          Container(
+            color: Colors.white,
+            margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+            padding: EdgeInsets.all(10.0),
+            child: Row(
+              children: <Widget>[
+                Icon(
+                  Icons.add_shopping_cart,
+                  color: Colors.teal.shade900,
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  '+44 123 456 789',
+                  style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0),
+                )
+              ],
+            ),
+          ),
+          Container(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              padding: EdgeInsets.all(10.0),
+              color: Colors.white,
+              child: Row(
+                children: <Widget>[
+                  Icon(Icons.email, color: Colors.teal.shade900),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text('chnrd.k@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                      ))
+                ],
+              ))
         ]),
       ),
     ));
